ff811894f86f351952e828793dfc4b4052ddc325
milda norkute <menulio.kiskis@gmail.com>
2019-01-02 19:24:07 +0200

    Updates

---
+++ precache-manifest.f08be74e27c03b6c421d041717667fa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ecbc2e165301c31fe8f",
+    "url": "/notes-panel/static/js/runtime~main.2ecbc2e1.js"
+  },
+  {
+    "revision": "845a68a2d8a17f433cc8",
+    "url": "/notes-panel/static/js/main.845a68a2.chunk.js"
+  },
+  {
+    "revision": "d11e2203d041bb24ddbd",
+    "url": "/notes-panel/static/js/1.d11e2203.chunk.js"
+  },
+  {
+    "revision": "845a68a2d8a17f433cc8",
+    "url": "/notes-panel/static/css/main.cce8833d.chunk.css"
+  },
+  {
+    "revision": "da6f3aa8b0e9c4e397969d6f56c0686d",
+    "url": "/notes-panel/index.html"
+  }
+];
